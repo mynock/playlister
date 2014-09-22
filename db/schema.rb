d29c704b8dec79075c82d3256b3c3638f521bcf9
Kevin Mehlbrech <mynock51@gmail.com>
2014-09-22 13:03:15 -0700

    Added songs to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922180154) do
+ActiveRecord::Schema.define(version: 20140922192307) do
 
   create_table "playlists", force: true do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140922180154) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "playlist_id"
   end
 
 end
